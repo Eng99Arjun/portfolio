c17291bfc06d02ef141489881a4034444755b844
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-04-03 14:04:08 +0530

    add Contact component and integrate framer-motion for animations; enhance Experience and Hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import profilePic from "../assets/raviKumarProfile.webp"
 import { HERO_CONTENT } from '../constants'
+import { motion, stagger } from 'framer-motion'
+
+
+const containerVariants = {
+    hidden:{opacity:0, x: -100},
+    visible: {
+        opacity:1,
+        x:0,
+        transition: {
+           duration:0.5,
+           staggerChildren: 0.5,
+        }
+    }
+}
+
+const childVariants = {
+    hidden: { opacity: 0, x: -100},
+    visible: { opacity:1, x:0, transition: { duration: 0.5 } } 
+}
 
 const Hero = () => {
   return (
@@ -9,11 +28,18 @@ const Hero = () => {
             <div className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:p-8">
                     
-                    <img src={profilePic} alt="Ravi Kumar" className='border border-stone-900 rounded-3xl' />
+                    <motion.img src={profilePic} 
+                    alt="Ravi Kumar" 
+                    width={650}
+                    height={650}
+                    className='border border-stone-900 rounded-3xl' 
+                    initial ={{x:100, opacity: 0}}
+                    animate= {{x:0, opacity: 1}}
+                    transition={{duration:1 , delay: 1.5}} />
                 </div>
             </div>
             <div className="w-full lg:w-1/2">
-                <div className="flex flex-col items-center lg:items-start mt-10">
+                <motion.div className="flex flex-col items-center lg:items-start mt-10">
                     <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">Ravi Kumar</h2>
                     <span  className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'> Full Stack Developer</span>
                     <p className='my-10 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>{HERO_CONTENT}</p>
@@ -22,7 +48,7 @@ const Hero = () => {
                         rel ='noopener noreferrer'
                         download
                         className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10 '>Download Resume</a>
-                </div>
+                </motion.div>
             </div>
             <div>
             </div>
